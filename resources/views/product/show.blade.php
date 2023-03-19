08514039a3592b4dd20037df8ebe7d1d94fcef7c
bduha184 <bduha_184@yahoo.co.jp>
2023-03-19 09:13:02 +0900

    商品詳細ページ作成 トップページから詳細ページ遷移設定

---
+++ resources/views/product/show.blade.php
@@ -0,0 +1,19 @@
+<x-app>
+    <x-slot name="title">
+        {{$product->name}}
+    </x-slot>
+    <div class="container">
+        <div class="product">
+            <img src="{{ asset($product->image) }}" class="product-img"/>
+            <div class="product__content-header text-center">
+                <div class="product__name">
+                    {{ $product->name }}
+                </div>
+                <div class="product__price">
+                    ¥{{ number_format($product->price) }}
+                </div>
+            </div>
+            {{ $product->description }}
+        </div>
+    </div>
+</x-app>
